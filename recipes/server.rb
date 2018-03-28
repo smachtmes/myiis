11ff85fcace234cacdcf76edcb66406046c7cd15
smachtmes <smachtmes@gmail.com>
2018-03-28 22:31:13 +0000

    0.2.1 using template

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,12 +2,8 @@ powershell_script 'Install IIS' do
   code 'Add-WindowsFeature Web-Server'
 end
 
-file 'C:\inetpub\wwwroot\Default.htm' do
-  content "<h1>Hello, world!</h1>
-  <h2>PLATFORM: #{node['platform']}</h2>
-  <h2>HOSTNAME: #{node['hostname']}</h2>
-  <h2>MEMORY: #{node['memory']['total']}</h2>
-  <h2>CPU mhz: #{node['cpu']['0']['mhz']}</h2>"
+template 'C:\inetpub\wwwroot\Default.htm' do
+  source 'Default.htm.erb'
 end
 
 service 'w3svc' do
